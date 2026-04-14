c6fc7f024a3a8b4cfdecb3269c41d686d1fd5db3
taehuikim <taehuikim9307@gmail.com>
2026-04-15 01:58:31 +0900

    feat: update editor styles and import additional theme styles

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -3,6 +3,17 @@ import { Crepe } from "@milkdown/crepe";
 import { initMermaid, getMermaidRenderPreview } from "./plugins/mermaidPlugin";
 import { useAppStore } from "../../stores/appStore";
 import "@milkdown/crepe/theme/frame.css";
+import "@milkdown/crepe/theme/common/style.css";
+import "@milkdown/crepe/theme/common/block-edit.css";
+import "@milkdown/crepe/theme/common/toolbar.css";
+import "@milkdown/crepe/theme/common/link-tooltip.css";
+import "@milkdown/crepe/theme/common/image-block.css";
+import "@milkdown/crepe/theme/common/table.css";
+import "@milkdown/crepe/theme/common/latex.css";
+import "@milkdown/crepe/theme/common/code-mirror.css";
+import "@milkdown/crepe/theme/common/list-item.css";
+import "@milkdown/crepe/theme/common/placeholder.css";
+import "@milkdown/crepe/theme/common/cursor.css";
 
 interface EditorProps {
   defaultValue?: string;
